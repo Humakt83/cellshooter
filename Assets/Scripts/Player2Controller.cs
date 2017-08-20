7c881d274d8b3cf7d30ed7c8e59cf9ab2e1c16cf
Humakt83 <urlact@yahoo.com>
2017-08-20 23:38:52 +0300

    Two player game
    Ship controller with mouse animates a bit funky.

---
+++ Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class Player2Controller : MonoBehaviour {
+
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private Rigidbody rBody;
+    private float nextFire = 0.0f;
+
+    private void Start() {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate () {
+        MoveShip();
+	}
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire) {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
+            
+        }
+    }
+
+    private void MoveShip() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal * speed, 0.0f, moveVertical * speed);
+        rBody.velocity = movement;
+
+        rBody.position = new Vector3(
+            Mathf.Clamp(rBody.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rBody.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rBody.rotation = Quaternion.Euler(0.0f, 0.0f, rBody.velocity.x * tilt);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class Boundary {
-    public float xMin, xMax, zMin, zMax;
-}
-
 public class PlayerController : MonoBehaviour {
 
     public float speed;
@@ -23,32 +18,36 @@ public class PlayerController : MonoBehaviour {
         rBody = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate () {
+    void FixedUpdate() {
         MoveShip();
-	}
+    }
 
     void Update() {
-        if (Input.GetButton("Fire1") && Time.time > nextFire) {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Time.time > nextFire) {
             nextFire = Time.time + fireRate;
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
             GetComponent<AudioSource>().Play();
-            
         }
     }
 
     private void MoveShip() {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Mathf.Abs(pz.x - rBody.position.x) < 0.3f && Mathf.Abs(pz.z - rBody.position.z) < 0.3f) {
+            rBody.velocity = new Vector3(0.0f, 0.0f, 0.0f);            
+        } else {
+            Vector3 movement = new Vector3(Mathf.Sign(pz.x - rBody.position.x) * speed, 0.0f, Mathf.Sign(pz.z - rBody.position.z) * speed);
+            rBody.velocity = movement;
 
-        Vector3 movement = new Vector3(moveHorizontal * speed, 0.0f, moveVertical * speed);
-        rBody.velocity = movement;
+            rBody.position = new Vector3(
+                Mathf.Clamp(rBody.position.x, boundary.xMin, boundary.xMax),
+                0.0f,
+                Mathf.Clamp(rBody.position.z, boundary.zMin, boundary.zMax)
+            );
+        }
+        RotateShip();
+    }
 
-        rBody.position = new Vector3(
-            Mathf.Clamp(rBody.position.x, boundary.xMin, boundary.xMax),
-            0.0f,
-            Mathf.Clamp(rBody.position.z, boundary.zMin, boundary.zMax)
-        );
-
+    private void RotateShip() {
         rBody.rotation = Quaternion.Euler(0.0f, 0.0f, rBody.velocity.x * tilt);
     }
 }
